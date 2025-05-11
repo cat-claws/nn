8c3b168dc60af93768191f39102952921677e198
cat-claws <47313357+cat-claws@users.noreply.github.com>
2025-05-11 17:51:58 +0800

    Update resnet_cifar.py

--- resnet_cifar.py
+++ resnet_cifar.py
@@ -13,28 +13,8 @@ https://github.com/WenRuiUSTC/EntF/blob/main/models/resnet.py
 import torch
 import torchvision
 
-# .models import resnet, resnet18, resnet34, resnet50, resnet101, resnet152
-
-# def _resnet(block, layers, **kwargs):
-    
-#     _model = torchvision.models.ResNet(block, layers, **kwargs)
-
-   
-
-# def get_resnet18_cifar():
-# 	_model = ResNet(block, layers, **kwargs)
-# 	_model.conv1 = torch.nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1, bias=False)
-# 	_model.maxpool = torch.nn.Identity()
-# 	_model.fc = torch.nn.Linear(512, 10)	
-# 	return _model
-# _resnet(BasicBlock, [2, 2, 2, 2], weights, progress, **kwargs)
-
 def get_resnet_cifar(block, layers, **kwargs):
-	if ('bottle' in block.lower() or 'neck' in block.lower()):
-		block = torchvision.models.resnet.Bottleneck
-	else:
-		block = torchvision.models.resnet.BasicBlock
-	print(block)
+	block = torchvision.models.resnet.Bottleneck if ('bottle' in block.lower() or 'neck' in block.lower()) else torchvision.models.resnet.BasicBlock
 	model = torchvision.models.ResNet(block, layers, **kwargs)
 	
 	model.conv1 = torch.nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1, bias=False)
