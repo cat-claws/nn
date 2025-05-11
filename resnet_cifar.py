070976029cd3d5f31d854357fab86989e94e6a64
cat-claws <47313357+cat-claws@users.noreply.github.com>
2025-05-11 17:54:38 +0800

    Update resnet_cifar.py

--- resnet_cifar.py
+++ resnet_cifar.py
@@ -1,9 +1,3 @@
-'''
-Reference:
-[1] Kaiming He, Xiangyu Zhang, Shaoqing Ren, Jian Sun
-    Deep Residual Learning for Image Recognition. arXiv:1512.03385
-'''
-
 """Resnet implementation is based on the implementation found in:
 https://github.com/YisenWang/MART/blob/master/resnet.py
 https://github.com/yaodongyu/TRADES/blob/master/models/resnet.py
@@ -20,8 +14,6 @@ def get_resnet_cifar(block, layers, **kwargs):
 	model.conv1 = torch.nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1, bias=False)
 	model.maxpool = torch.nn.Identity()  # Remove maxpool
 	
-	# model.fc = torch.nn.Linear(512, kwargs["num_classes"])
-
 	# Optional: modify BasicBlock if you don't want to remove ReLU after bn1
 	if not bool(kwargs.get("relu", False)):
 		for m in model.modules():
