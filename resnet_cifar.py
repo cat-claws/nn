104d7ac75dcea29fc9fc852de426117b30290fc1
cat-claws <47313357+cat-claws@users.noreply.github.com>
2025-05-11 17:40:29 +0800

    Update resnet_cifar.py

--- resnet_cifar.py
+++ resnet_cifar.py
@@ -30,6 +30,7 @@ import torchvision
 # _resnet(BasicBlock, [2, 2, 2, 2], weights, progress, **kwargs)
 
 def get_resnet_cifar(block, layers, **kwargs):
+	block = torchvision.models.resnet.Bottleneck if 'bottle' in block.lower() or 'neck' in block.lower() else torchvision.models.resnet.BasicBlock
 	model = torchvision.models.ResNet(block, layers, **kwargs)
 	
 	model.conv1 = torch.nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1, bias=False)
